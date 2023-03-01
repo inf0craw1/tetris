0ad8fa7036a3d1e8a438850972c2eab92d9e76df
inf0craw1 <inf0craw1@naver.com>
2023-03-01 23:43:55 +0900

    feat: current block rotating

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,16 +1,47 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { GameContext } from "../contexts/GameContext";
 import { GridContext } from "../contexts/GridContext";
 import "../styles/display.scss";
 
+type CurrentBlockType = {
+  blockType: number;
+  direction: number;
+  location: {
+    x: number;
+    y: number;
+  };
+};
+
 function Display() {
+  const gameData = useContext(GameContext);
   const gridData = useContext(GridContext);
-  const [currentBlock, setCurrentBlock] = useState({
+  const [currentBlock, setCurrentBlock] = useState<CurrentBlockType>({
     blockType: 1,
     direction: 0,
     location: {
       x: 4,
       y: 0,
     },
+  });
+
+  const windowKeyListener = (e: KeyboardEvent) => {
+    console.log(e);
+    if (e.key === " ") {
+      setCurrentBlock((prev) => {
+        return {
+          ...prev,
+          direction: prev.direction === 3 ? 0 : prev.direction + 1,
+        };
+      });
+    }
+  };
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    window.addEventListener("keydown", windowKeyListener, {
+      signal: abortController.signal,
+    });
+    return () => abortController.abort();
   });
 
   const renderGrid = () =>
@@ -22,7 +53,42 @@ function Display() {
       </div>
     ));
 
-  return <div className="display">{renderGrid()}</div>;
+  const renderCurrentBlock = () => (
+    <div
+      className="current-block-container"
+      style={{
+        top: currentBlock.location.y * gameData.boxSize,
+        left: currentBlock.location.x * gameData.boxSize,
+      }}
+    >
+      {gameData.block1.shapeOfDirection[currentBlock.direction].map(
+        (row, i) => (
+          <div key={`currentBlockGridRow${i}`} className="grid-row">
+            {row.map((col, j) => (
+              <div
+                key={`currentBlockGridItem${i}${j}`}
+                className="grid-item"
+                style={
+                  gameData.block1.shapeOfDirection[currentBlock.direction][i][j]
+                    ? { backgroundColor: gameData.block1.color }
+                    : {}
+                }
+              ></div>
+            ))}
+          </div>
+        )
+      )}
+    </div>
+  );
+
+  return (
+    <div className="display">
+      <>
+        {renderGrid()}
+        {renderCurrentBlock()}
+      </>
+    </div>
+  );
 }
 
 export default Display;
