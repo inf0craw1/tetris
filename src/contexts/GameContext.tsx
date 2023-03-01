dc75bcacdcf90a9c0d1e1b041ad3a77aa2ea8c21
inf0craw1 <inf0craw1@naver.com>
2023-03-01 22:48:58 +0900

    feat: add block data

--- src/contexts/GameContext.tsx
+++ src/contexts/GameContext.tsx
@@ -2,7 +2,37 @@ import { createContext } from "react";
 
 export const gameContextData = {
   horizontalBoxCount: 10,
-  verticalBoxCount: 30,
+  verticalBoxCount: 20,
+  block1: {
+    color: "#aaaaff",
+    direction: [
+      [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
+      [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+      ],
+
+      [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+      ],
+      [
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+      ],
+    ],
+  },
 };
 
 export const GameContext = createContext(gameContextData);
